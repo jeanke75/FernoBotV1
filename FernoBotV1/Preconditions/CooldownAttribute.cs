296590544fcdc46e1718879d31835cb81ed3802f
appelemac <appelemac@gmail.com>
2017-03-16 15:39:49 +0100

    cooldownattribute

---
+++ FernoBotV1/Preconditions/CooldownAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace FernoBotV1.Preconditions
+{
+    public class CooldownAttribute : PreconditionAttribute
+    {
+        public TimeSpan CooldownTime { get; }
+
+        public Dictionary<ulong, DateTimeOffset> CoolingDown { get; set; }
+
+        public CooldownAttribute(int hours, int minutes, int seconds)
+        {
+            CooldownTime = new TimeSpan(hours, minutes, seconds);
+            CoolingDown = new Dictionary<ulong, DateTimeOffset>();
+        }
+
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
+        {
+            if (CoolingDown.ContainsKey(context.User.Id))
+            {
+
+            }
+            CoolingDown.
+        }
+    }
+}
