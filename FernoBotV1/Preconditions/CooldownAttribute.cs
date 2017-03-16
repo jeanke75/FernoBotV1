3e41bb91146bfc4bc9cc88167ecae2cb203d065e
appelemac <appelemac@gmail.com>
2017-03-16 15:48:23 +0100

    Cooldown framework donw

--- FernoBotV1/Preconditions/CooldownAttribute.cs
+++ FernoBotV1/Preconditions/CooldownAttribute.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using System.Collections.Concurrent;
 
 namespace FernoBotV1.Preconditions
 {
@@ -11,21 +12,34 @@ namespace FernoBotV1.Preconditions
     {
         public TimeSpan CooldownTime { get; }
 
-        public Dictionary<ulong, DateTimeOffset> CoolingDown { get; set; }
+        public ConcurrentDictionary<ulong, DateTimeOffset> CoolingDown { get; set; }
 
         public CooldownAttribute(int hours, int minutes, int seconds)
         {
             CooldownTime = new TimeSpan(hours, minutes, seconds);
-            CoolingDown = new Dictionary<ulong, DateTimeOffset>();
+            CoolingDown = new ConcurrentDictionary<ulong, DateTimeOffset>();
+            //optional extra todo: start timer for regular cleanup
         }
 
         public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
         {
             if (CoolingDown.ContainsKey(context.User.Id))
             {
-
+                DateTimeOffset endTime;
+                if (CoolingDown.TryGetValue(context.User.Id, out endTime))
+                {
+                    if (endTime > DateTimeOffset.Now)
+                        return Task.FromResult(PreconditionResult.FromError("not cooled down yet"));
+                }
+                else
+                {
+                    //something really glitchy happened, timer interfered?
+                    //for now we'll ignore this possibility except for logging it
+                    Console.WriteLine("glitchy cooldown error occurred.");
+                }
             }
-            CoolingDown.
+            CoolingDown.AddOrUpdate(context.User.Id, DateTimeOffset.Now + CooldownTime, (key, val) => DateTimeOffset.Now + CooldownTime);
+            return Task.FromResult(PreconditionResult.FromSuccess());
         }
     }
 }
