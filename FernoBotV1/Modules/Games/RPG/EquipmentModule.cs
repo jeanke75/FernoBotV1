10df336e70508725ba4afe19ad04518548e96902
appelemac <appelemac@gmail.com>
2017-03-18 22:04:48 +0100

    item extension, refinement of equipment

--- FernoBotV1/Modules/Games/RPG/EquipmentModule.cs
+++ FernoBotV1/Modules/Games/RPG/EquipmentModule.cs
@@ -9,7 +9,6 @@ namespace FernoBotV1.Modules.Games.RPG
 {
     public class EquipmentModule : ModuleBase
     {
-       public static List<string> DefaultItems = new List<string> { "wooden sword" };
 
         /*[Command("Equip")]
         [Summary("Equip an item from your inventory")]
@@ -93,36 +92,45 @@ namespace FernoBotV1.Modules.Games.RPG
 
         private async static Task EquipItemAsync(SqlConnection conn, SqlTransaction tr, long userId, Item item)
         {
-            
+
             switch (item.type)
             {
                 case ItemType.Weapon:
                     await EquipWeaponAsync(conn, tr, userId, item as Weapon);
                     break;
                 case ItemType.Armor:
+                    string colName = string.Empty;
                     switch ((item as Armor).subtype)
                     {
                         case ArmorType.Helmet:
-                            await EquipHelmetAsync(conn, tr, userId, item as Armor);
+                            colName = "HelmetID";
                             break;
                         case ArmorType.Upper:
-                            await EquipUpperAsync(conn, tr, userId, item as Armor);
+                            colName = "UpperID";
                             break;
                         case ArmorType.Pants:
-                            await EquipPantsAsync(conn, tr, userId, item as Armor);
+                            colName = "PantsID";
                             break;
                         case ArmorType.Boots:
-                            await EquipBootsAsync(conn, tr, userId, item as Armor);
+                            colName = "BootsID";
                             break;
                         case ArmorType.Gauntlets:
-                            await EquipGauntletsAsync(conn, tr, userId, item as Armor);
+                            colName = "GloveID";
                             break;
                         case ArmorType.Mantle:
-                            await EquipMantleAsync(conn, tr, userId, item as Armor);
+                            colName = "MantleID";
                             break;
                         case ArmorType.Shield:
-                            await EquipShieldAsync(conn, tr, userId, item as Armor);
+                            colName = "ShieldID";
                             break;
+                    }
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.Transaction = tr;
+                        cmd.Parameters.AddWithValue("@user", userId);
+                        cmd.Parameters.AddWithValue("@item", ((Armor)item).id);
+                        cmd.CommandText = $"update Equipped set {colName} = @item where UserID = @user";
+                        await cmd.ExecuteNonQueryAsync();
                     }
                     break;
             }
@@ -140,88 +148,6 @@ namespace FernoBotV1.Modules.Games.RPG
             }
         }
 
-        private async static Task EquipHelmetAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor helmet)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = helmet.id;
-                cmd.CommandText = "update Equipped set HelmetID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
 
-        private async static Task EquipUpperAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor upper)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = upper.id;
-                cmd.CommandText = "update Equipped set UpperID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
-
-        private async static Task EquipPantsAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor pants)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = pants.id;
-                cmd.CommandText = "update Equipped set PantsID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
-
-        private async static Task EquipBootsAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor boots)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = boots.id;
-                cmd.CommandText = "update Equipped set BootsID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
-
-        private async static Task EquipGauntletsAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor gauntlets)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = gauntlets.id;
-                cmd.CommandText = "update Equipped set GloveID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
-
-        private async static Task EquipMantleAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor mantle)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = mantle.id;
-                cmd.CommandText = "update Equipped set MantleID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
-
-        private async static Task EquipShieldAsync(SqlConnection conn, SqlTransaction tr, long userId, Armor shield)
-        {
-            using (SqlCommand cmd = conn.CreateCommand())
-            {
-                cmd.Transaction = tr;
-                cmd.Parameters.Add("@user", DbType.Int64).Value = userId;
-                cmd.Parameters.Add("@item", DbType.Int32).Value = shield.id;
-                cmd.CommandText = "update Equipped set ShieldID = @item where UserID = @user";
-                await cmd.ExecuteNonQueryAsync();
-            }
-        }
     }
 }
